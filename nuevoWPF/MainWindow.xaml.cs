d5dc4f7cb08e5de59445903f50edb047278dabc6
dotCero <lucero.inginfo@gmail.com>
2021-10-13 16:29:28 -0300

    Agregar archivos de proyecto.

---
+++ nuevoWPF/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace nuevoWPF
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        Grid miGrid;
+        TextBox miTextBox;
+        Button miBoton;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            miGrid = new Grid();
+            miTextBox = new TextBox();
+            miBoton = new Button();
+
+            miTextBox.Width = 200;
+            miTextBox.Height = 50;
+            miBoton.Width = 100;
+            miBoton.Height = 50;
+            miBoton.Margin = new Thickness(0, 120, 0, 0);
+            miBoton.Click += button_Click;
+
+
+            miGrid.Children.Add(miTextBox);
+            miGrid.Children.Add(miBoton);
+
+            this.AddChild(miGrid);
+
+            this.ResizeMode = ResizeMode.NoResize;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            string cajaDeTexto = miTextBox.Text;
+            this.Title = cajaDeTexto;
+        }
+    }
+}
---
+++ nuevoWPF/Persona.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nuevoWPF
+{
+    class Persona
+    {
+        private string nombre;
+        private string rut;
+        private int edad;
+
+        public Persona(string nombre, string rut, int edad)
+        {
+            this.nombre = nombre;
+            this.rut = rut;
+            this.edad = edad;
+        }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Rut { get => rut; set => rut = value; }
+        public int Edad { get => edad; set => edad = value; }
+
+        public override string ToString()
+        {
+            string mostrar = String.Format("{0}, {1}, {2}", Nombre, Rut, Edad);
+            return mostrar;
+        }
+    }
+}
